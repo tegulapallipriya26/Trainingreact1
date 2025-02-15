2db50ea85b09b080cb0223a91c2dba587d019875
Tegulapalli Saipriya <2211cs010560mallareddyuniversity.ac.in>
2025-02-15 12:56:08 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,60 @@
-import React from 'react'
-import Navbar from './components/Navbar';
-import {BrowserRouter  , Routes , Route} from "react-router-dom";
-import Dashboard from './pages/Dashboard';
-import Clasroom from './pages/Clasroom';
-import Assessments from './pages/Assessments';
-import Store from './pages/Store';
-import Calender from './pages/Calender';
-import News from './pages/News';
-import Blog from './pages/Blog';
-import PageNotFound from './pages/PageNotFound';
-import Profile from './pages/Profile';
-import MyAccount from './pages/MyAccount';
-import MySettings from './pages/MySettings';
-const App = () => {
+// import React from 'react'
+// import Navbar from './components/Navbar';
+// import {BrowserRouter  , Routes , Route} from "react-router-dom";
+// import Dashboard from './pages/Dashboard';
+// import Clasroom from './pages/Clasroom';
+// import Assessments from './pages/Assessments';
+// import Store from './pages/Store';
+// import Calender from './pages/Calender';
+// import News from './pages/News';
+// import Blog from './pages/Blog';
+// import PageNotFound from './pages/PageNotFound';
+// import Profile from './pages/Profile';
+// import MyAccount from './pages/MyAccount';
+// import MySettings from './pages/MySettings';
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//         <Navbar/>
+//         <Routes>
+//             <Route path='/' element={<Dashboard/>}/>
+//             <Route path='/classroom' element={<Clasroom/>}/>
+//             <Route path='/assessment' element={<Assessments/>}/>
+//             <Route path='/store' element={<Store/>}/>
+//             <Route path='/calender' element={<Calender/>}/>
+//             <Route path='/news' element={<News/>}/>
+//             <Route path='/blog' element={<Blog/>}/>
+//             <Route path='/profile' element={<Profile/>}>
+//               <Route path='/profile/myaccount' element={<MyAccount/>}/>
+//               <Route path='/profile/mysettings' element={<MySettings/>}/>
+//               </Route>
+//             <Route path='*' element={<PageNotFound/>}/>
+//         </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
+// import React from 'react'
+// import {BrowserRouter , Routes , Route} from "react-router-dom"
+// import Home from './crudcomponents/Create';
+// import Create from './crudcomponents/'
+
+// const App = () => {
+//   return (
+//     <div>App</div>
+//   )
+// }
+// export default AppS
+import React from "react";
+import Card from "./Card"; // Import the Card component
+
+function App() {
   return (
-    <BrowserRouter>
-        <Navbar/>
-        <Routes>
-            <Route path='/' element={<Dashboard/>}/>
-            <Route path='/classroom' element={<Clasroom/>}/>
-            <Route path='/assessment' element={<Assessments/>}/>
-            <Route path='/store' element={<Store/>}/>
-            <Route path='/calender' element={<Calender/>}/>
-            <Route path='/news' element={<News/>}/>
-            <Route path='/blog' element={<Blog/>}/>
-            <Route path='/profile' element={<Profile/>}>
-              <Route path='/profile/myaccount' element={<MyAccount/>}/>
-              <Route path='/profile/mysettings' element={<MySettings/>}/>
-              </Route>
-            <Route path='*' element={<PageNotFound/>}/>
-        </Routes>
-    </BrowserRouter>
-  )
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", background: "#f5f5f5" }}>
+      <Card />
+    </div>
+  );
 }
 
-export default App
+export default App;
